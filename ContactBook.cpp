9beb473473da99c591565aaa189e7fc8400c9c85
Orlando Vargas <Orlandov87@live.com>
2021-03-23 21:45:58 -0400

    Updated a few functions for requirements

--- ContactBook.cpp
+++ ContactBook.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include "ContactBook.h"
 
-class ContactBook {
+ContactBook::ContactBook() {
+    ownerFirstName = "";
+    ownerLastName = "";
+    contactBook[10];
+    bookSize = 0;      
+}
 
-};
+ContactBook::ContactBook(string lName, string fName) {
+    ownerFirstName = fName;
+    ownerLastName = lName;
+    contactBook[10];
+    bookSize = 0;
+}
+
+string ContactBook::getOwnerLastName() {
+    return ownerLastName;
+}
+
+string ContactBook::getOwnerFirstName() {
+    return ownerFirstName;
+}
+
+void ContactBook::setOwnerLastName(string lName) {
+    ownerLastName = lName;
+}
+
+void ContactBook::setOwnerLastName(string fName) {
+    ownerFirstName = fName;
+}
+
+int ContactBook::addNewContact(Contact newContact) {
+    if(bookSize < 10) {
+        contactBook[bookSize] = newContact;
+        bookSize++;
+        return 0;
+    } else {
+        return 1;
+    }
+}
--- ContactBook.h
+++ ContactBook.h
@@ -1,17 +1,32 @@
 #include <iostream> 
+#include <string>
 #include "Contact.h"
 
 using std::string;
 
 class ContactBook {
+    private:
+        string ownerLastName;
+        string ownerFirstName;
+        Contact contactBook[10];
+        int bookSize;
+
     public:
         ContactBook();
         ContactBook(string ownerLastName, string ownerFirstName);
 
+        string getOwnerLastName();
+        string getOwnerFirstName();
+        
+        void setOwnerLastName(string lName);
+        void setOwnerFirstname(string fName);
 
-    private:
-        string ownerLastName;
-        string ownerFirstName;
-        Contact contactBook;
+        int addNewContact(Contact contact);
+        //MARK: Check Point
+        //TODO: Define these functions
+        void deleteContact();
+        void displayContactInfo();
+        void updateContactInfo();
+        void displayAllContacts();
 
 };
